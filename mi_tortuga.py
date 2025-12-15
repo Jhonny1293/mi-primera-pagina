2d4af6b8845b1eaa868998c9e95514e161a0f8e8
Jhonatan Restrepo Montoya <124320588+Jhonny1293@users.noreply.github.com>
2025-12-14 19:06:42 -0500

    finalizar código de los retos de la actividad 2

--- mi_tortuga.py
+++ mi_tortuga.py
@@ -1,15 +1,15 @@
 #Reto1
-""" print("Simulador del movimiento de una tortuga")
+print("Simulador del movimiento de una tortuga")
 pasos_adelante = int(input("Ingrese la cantidad de pasos hacia adelante: "))
-print("-" * pasos_adelante + ">") """
+print("-" * pasos_adelante + ">")
 
 #Reto2
-""" pasos_abajo = int(input("Ingrese la cantidad de pasos hacia abajo: "))
+pasos_abajo = int(input("Ingrese la cantidad de pasos hacia abajo: "))
 for _ in range(pasos_abajo):
-    print("|" + "v") """
+    print("|" + "v")
 
 #Reto3
-""" pasos_derecha = int(input("¿Cuántos pasos deseas mover hacia la derecha? "))
+pasos_derecha = int(input("¿Cuántos pasos deseas mover hacia la derecha? "))
 pasos_abajo = int(input("¿Cuántos pasos deseas mover hacia abajo? "))
 
 # Línea horizontal con el giro
@@ -17,11 +17,11 @@ print("-" * pasos_derecha + "|")
 
 # Línea vertical hacia abajo
 for _ in range(pasos_abajo - 1):
-    print(" " * pasos_derecha + "|") """
+    print(" " * pasos_derecha + "|")
 
 
 #Reto4
-""" def mover_derecha(pasos):
+def mover_derecha(pasos):
     # Dibuja la línea horizontal y el giro hacia abajo
     print("-" * pasos + "|")
     
@@ -32,41 +32,32 @@ def mover_abajo(pasos, desplazamiento):
         print(" " * desplazamiento + "|")
 
 
-# --- Programa principal ---
-
 # Primero pedimos los pasos
 pasos_derecha = int(input("¿Cuántos pasos mover hacia la derecha? "))
 pasos_abajo = int(input("¿Cuántos pasos mover hacia abajo? "))
 
 # Ahora llamamos a las funciones
 mover_derecha(pasos_derecha)
-mover_abajo(pasos_abajo, pasos_derecha) """
+mover_abajo(pasos_abajo, pasos_derecha)
 
 #Reto5
-# Variable global para recordar la posición actual en X
-posicion_horizontal = 0
-
+x = 0
 
 def mover_derecha(pasos):
-    global posicion_horizontal
-    # aumentamos la posición acumulada
-    posicion_horizontal += pasos
-    
-    # dibujamos la línea horizontal del escalón
-    print("-" * pasos + "|")
-
+    global x
+    print(" " * x + "-" * pasos + "|")
+    x += pasos
 
 def mover_abajo(pasos):
-    # usamos la posición acumulada para alinear verticalmente
+    global x
     for _ in range(pasos - 1):
-        print(" " * posicion_horizontal + "|")
+        print(" " * x + "|")
 
-# Entradas para el usuario
-cantidad_escalones = int(input("¿Cuántos escalones deseas bajar? "))
+mover_derecha(4)
+mover_abajo(4)
 
-for i in range(cantidad_escalones):
-    pasos_derecha = int(input(f"Pasos hacia la derecha del escalón {i+1}: "))
-    pasos_abajo = int(input(f"Pasos hacia abajo del escalón {i+1}: "))
-    
-    mover_derecha(pasos_derecha)
-    mover_abajo(pasos_abajo)
+mover_derecha(5)
+mover_abajo(5)
+
+mover_derecha(6)
+mover_abajo(6)
